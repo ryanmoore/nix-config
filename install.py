70f5e2620db666623f5222b52ef6edce62f4deb0
Ryan Moore <ryanmoore88@gmail.com>
2013-12-06 18:42:18 -0500

    Removed Xdefaults which as been replaced by xresources. Changed DIR_COLORS to install to ~/.dir_colors instead of system wide. Added --force option to avoid prompts. Fixed issue where files would be skipped if non-existant.

--- install.py
+++ install.py
@@ -6,6 +6,7 @@ import errno
 import shutil
 import subprocess
 import logging
+import argparse
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 EXTERNAL = os.path.join(BASE_DIR, 'external')
@@ -29,24 +30,28 @@ def git_clone(repo, root):
         logging.info( 'from {}: {}'.format(cwd, ' '.join(cmd)) )
     subprocess.check_call(cmd, cwd=cwd)
 
-def setup_pathogen():
+def setup_pathogen(args):
     git_clone(pathogen_repo, pathogen_root)
     src = os.path.join(pathogen_root, 'autoload', 'pathogen.vim')
     dest = os.path.join(VIM_DIR, 'autoload', 'pathogen.vim')
-    install(src, dest)
+    install(src, dest, args)
 
-def setup_solarized():
+def setup_solarized(args):
     git_clone(solarized_repo, solarized_root)
     solarized_colors_dir = 'vim-colors-solarized'
     src = os.path.join(solarized_root, solarized_colors_dir)
     dest = os.path.join(VIM_DIR, 'bundle', solarized_colors_dir)
-    install(src, dest)
+    install(src, dest, args)
 
 def main():
     logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(description='Install config files to home directory (with soft links)')
+    parser.add_argument('--force', '-f', action='store_true', default=False,
+            help='Don\'t ask about overwriting files, just do.')
 
-    setup_pathogen()
-    setup_solarized()
+    args = parser.parse_args()
+    setup_pathogen(args)
+    setup_solarized(args)
 
     tolink = [
              ('bashrc',    '~/.bashrc'),
@@ -55,6 +60,7 @@ def main():
              ('screenrc',  '~/.screenrc'),
              ('config',    '~/.config'),
              ('tmux.conf', '~/.tmux.conf'),
+             ('dir_colors','~/.dir_colors'),
             ]
 
     src_folder = os.path.abspath(os.path.join(BASE_DIR, 'src'))
@@ -63,27 +69,28 @@ def main():
         os.path.expanduser('~/.Xresources') ) )
 
     for src,dest in tolink:
-        install( src, dest )
+        install( src, dest, args )
 
 def user_says_yes(query):
     answer = input(query)
     return answer.lower() == 'y'
 
-def install( src, dest ):
+def install( src, dest, args ):
     src = os.path.abspath(src)
     dest = os.path.abspath(dest)
-    if os.path.exists(dest) and user_says_yes('Overwrite {} (Y/n)? '.format(dest) ):
-        logging.info('rm {}'.format(dest))
-        try:
-            os.remove(dest)
-        except OSError as e:
-            if e.errno == os.errno.EISDIR:
-                shutil.rmtree(dest)
-            else:
-                raise
-    else:
-        logging.info('skipping {}'.format(dest))
-        return
+    if os.path.exists(dest):
+        if args.force or user_says_yes( 'Overwrite {} (Y/n)? '.format(dest) ):
+            logging.info('rm {}'.format(dest))
+            try:
+                os.remove(dest)
+            except OSError as e:
+                if e.errno == os.errno.EISDIR:
+                    shutil.rmtree(dest)
+                else:
+                    raise
+        else:
+            logging.info('skipping {}'.format(dest))
+            return
     logging.info('ln -s {} {}'.format(src, dest))
     os.symlink( src, dest )
 
