0c5478f953f63da38dfdfad02eacbe777f283f3e
Ryan Moore <ryanmoore88@gmail.com>
2014-12-07 12:09:55 -0500

    Added font selection to rc.lua for awesome configuration.

--- src/config/awesome/rc.lua
+++ src/config/awesome/rc.lua
@@ -379,3 +379,5 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+awesome.font = "inconsolata 14"
